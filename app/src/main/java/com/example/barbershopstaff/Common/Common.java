54cf60f91a5b93e03a423e9148896140eadc6bcf
kayub007 <kayub007@yahoo.com>
2019-07-05 21:24:40 +0100

    initial commit

---
+++ app/src/main/java/com/example/barbershopstaff/Common/Common.java
@@ -0,0 +1,5 @@
+package com.example.barbershopstaff.Common;
+
+public class Common {
+    public static String State_name = "";
+}
---
+++ app/src/main/java/com/example/barbershopstaff/SalonListActivity.java
@@ -0,0 +1,13 @@
+package com.example.barbershopstaff;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class SalonListActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_salon_list);
+    }
+}
